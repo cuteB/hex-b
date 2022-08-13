155b84d03738329aa2ba6ee68f734c41ec28ab0c
Brandon Godley <bgodley@microsoft.com>
2022-08-13 13:40:20 -0700

    fix: added get keys function that returns dict keys in order

--- hexBoy/models/SortedDict.py
+++ hexBoy/models/SortedDict.py
@@ -1,110 +1,117 @@
 """Dict actually working well. Maybe a bit of love to make it more readable"""
 
-'''----------------------------------
+"""----------------------------------
 Sorted Dict
-----------------------------------'''
-class SortedDict(dict):
-  _dictionary   = None  # the dict
-  _sortedItems  = None  # sorted list of the items based on the given value
-  _reverse      = None  # which way to sort the list
-  _getSortValue = None  # which value to use to sort the list
-
-  def __init__(self, dict = None, getSortValue = None, reverse = False):
-    dict.__init__(self)
-
-    self._sortedItems = []
-    self._reverse = reverse
-
-    # Set the getSortValue function
-    if (getSortValue == None):
-      self._getSortValue = self._defaultGetSortValue
-    else:
-      self._getSortValue = getSortValue
-
-    # Init the dict
-    if (dict == None):
-      self._dictionary = {}
-    else:
-      self._dictionary = dict
-      self._appendDict(dict)
-      self._sortItems()
-
-  '''---
-  Overide dict functions
-  ---'''
-  # dict[key] = value
-  def __setitem__(self, key, value):
-    self._addItem(key, value)
-    self._sortItems()
-
-  # del(dict[key])
-  def __delitem__(self, key):
-    super(SortedDict, self).__delitem__(key)
-
-    # update sortedItems with removed item
-    for ind, item in enumerate(self._sortedItems):
-      if item[0] == key:
-        del(self._sortedItems[ind])
-        break
-
-  '''---
-  Private functions
-  ---'''
-  # Add item to dict, update sorted items
-  def _addItem(self, key, value):
-    if (key in self):
-      # Key in dict already, update entry
-      super(SortedDict, self).__setitem__(key, value)
-
-      # update sortedItems with new value
-      for ind, item in enumerate(self._sortedItems):
-        if item[0] == key:
-          self._sortedItems[ind] = (key,value)
-          break
-
-    else:
-      # Key not in dict, add item
-      super(SortedDict, self).__setitem__(key, value)
-      self._sortedItems.append((key, value))
-
-  # Combine external dict into self, update sorted items
-  def _appendDict(self, dict):
-    for key in dict:
-      print(key)
-      self._addItem(key, dict[key])
-
-    self._sortItems()
-
-  # Sort all of the items in the dict based on the sort value
-  def _sortItems(self):
-    self._sortedItems.sort(key=self._getSortValue, reverse=self._reverse)
-
-  # Grab the value that will be used to sort the dict items
-  def _defaultGetSortValue(self, item):
-    return item[0] # Just use the key if no other getValue is provided
-
-  '''---
-  Public functions
-  ---'''
-  # pop off the lowest item (first item in sortedItems)
-  def popItem(self):
-    if (len(self) == 0):
-      return None
-
-    else:
-      key, value = self._sortedItems[0]
-      del(self[key])
-      return key, value
-
-  def hasKey(self, key):
-    return (key in self)
-
-  def getDict(self):
-    return self._dictionary
-
-'''
+----------------------------------"""
+"""
 Time Complexity
   get O(1)
   del O(n)
   set O(n)
-'''
+"""
+
+class SortedDict(dict):
+    _dictionary: dict = None  # the dict
+    _sortedItems: list = None  # sorted list of the items based on the given value
+    _reverse: bool  = None  # which way to sort the list
+    _getSortValue: callable = None  # which value to use to sort the list
+
+    def __init__(self, dict=None, getSortValue=None, reverse=False):
+        dict.__init__(self)
+
+        self._sortedItems = []
+        self._reverse = reverse
+
+        # Set the getSortValue function
+        if getSortValue == None:
+            self._getSortValue = self._defaultGetSortValue
+        else:
+            self._getSortValue = getSortValue
+
+        # Init the dict
+        if dict == None:
+            self._dictionary = {}
+        else:
+            self._dictionary = dict
+            self._appendDict(dict)
+            self._sortItems()
+
+    """---
+    Override dict functions
+    ---"""
+    # dict[key] = value
+    def __setitem__(self, key, value):
+        self._addItem(key, value)
+        self._sortItems()
+
+    # del(dict[key])
+    def __delitem__(self, key):
+        super(SortedDict, self).__delitem__(key)
+
+        # update sortedItems with removed item
+        for ind, item in enumerate(self._sortedItems):
+            if item[0] == key:
+                del self._sortedItems[ind]
+                break
+
+    """---
+    Private functions
+    ---"""
+    # Add item to dict, update sorted items
+    def _addItem(self, key, value):
+        if key in self:
+            # Key in dict already, update entry
+            super(SortedDict, self).__setitem__(key, value)
+
+            # update sortedItems with new value
+            for ind, item in enumerate(self._sortedItems):
+                if item[0] == key:
+                    self._sortedItems[ind] = (key, value)
+                    break
+
+        else:
+            # Key not in dict, add item
+            super(SortedDict, self).__setitem__(key, value)
+            self._sortedItems.append((key, value))
+
+    # Combine external dict into self, update sorted items
+    def _appendDict(self, dict):
+        for key in dict:
+            print(key)
+            self._addItem(key, dict[key])
+
+        self._sortItems()
+
+    # Sort all of the items in the dict based on the sort value
+    def _sortItems(self):
+        self._sortedItems.sort(key=self._getSortValue, reverse=self._reverse)
+
+    # Grab the value that will be used to sort the dict items
+    def _defaultGetSortValue(self, item):
+        return item[0]  # Just use the key if no other getValue is provided
+
+    """---
+    Public functions
+    ---"""
+    # pop off the lowest item (first item in sortedItems)
+    def popItem(self):
+        if len(self) == 0:
+            return None
+
+        else:
+            key, value = self._sortedItems[0]
+            del self[key]
+            return key, value
+
+    def hasKey(self, key):
+        return key in self
+
+    def getDict(self):
+        return self._dictionary
+
+    def getKeys(self):
+        keys = []
+        for i in self._sortedItems:
+            keys.append(i[0])
+
+        return keys
--- test/models/test_SortedDict.py
+++ test/models/test_SortedDict.py
@@ -3,97 +3,148 @@ from hexBoy.models.SortedDict import SortedDict
 
 @pytest.fixture(autouse=True)
 def before_and_after_test(tmpdir):
-  """Reset the board and pathfinder before each test"""
-  tmpdir.dict = SortedDict()
+    """Reset the board and pathfinder before each test"""
+    tmpdir.dict = SortedDict()
 
-  # ^^^ before ^^^
-  yield # run the rest
-  # vvv After vvv
+    # ^^^ before ^^^
+    yield  # run the rest
+    # vvv After vvv
+
 
 def test_CreateAndPopItems(tmpdir):
-  """Dict should allow set, pop and check if it has a key"""
-  key = 0
-  item = 0
-  tmpdir.dict[key] = item
+    """Dict should allow set, pop and check if it has a key"""
+    key = 0
+    item = 0
+    tmpdir.dict[key] = item
 
-  assert tmpdir.dict.hasKey(key)
+    assert tmpdir.dict.hasKey(key)
 
-  dictItem = tmpdir.dict.popItem()
-  assert dictItem == (key, item)
+    dictItem = tmpdir.dict.popItem()
+    assert dictItem == (key, item)
 
-  assert not tmpdir.dict.hasKey(key)
+    assert not tmpdir.dict.hasKey(key)
 
-  dictItem = tmpdir.dict.popItem()
-  assert dictItem == None
+    dictItem = tmpdir.dict.popItem()
+    assert dictItem == None
+
 
 def test_AppendDict(tmpdir):
-  """Should be able create with an existing dict and SortedDict"""
-  putMeInCoach = {
-    1: 1,
-    2: 2,
-    3: 3,
-  }
+    """Should be able create with an existing dict and SortedDict"""
+    putMeInCoach = {
+        1: 1,
+        2: 2,
+        3: 3,
+    }
 
-  tmpdir.dict = SortedDict(dict = putMeInCoach)
-  assert tmpdir.dict.hasKey(1)
-  assert tmpdir.dict.hasKey(2)
-  assert tmpdir.dict.hasKey(3)
+    tmpdir.dict = SortedDict(dict=putMeInCoach)
+    assert tmpdir.dict.hasKey(1)
+    assert tmpdir.dict.hasKey(2)
+    assert tmpdir.dict.hasKey(3)
 
-  sd = SortedDict(dict = putMeInCoach)
-  tmpdir.dict = SortedDict(dict = sd.getDict())
-  assert tmpdir.dict.hasKey(1)
-  assert tmpdir.dict.hasKey(2)
-  assert tmpdir.dict.hasKey(3)
+    sd = SortedDict(dict=putMeInCoach)
+    tmpdir.dict = SortedDict(dict=sd.getDict())
+    assert tmpdir.dict.hasKey(1)
+    assert tmpdir.dict.hasKey(2)
+    assert tmpdir.dict.hasKey(3)
+
+
 
 def test_SortItemsInDict(tmpdir):
-  """Should default sort items by their key"""
-  tmpdir.dict[1] = 1
-  tmpdir.dict[0] = 0
-  tmpdir.dict[2] = 2
+    """Should default sort items by their key"""
+    tmpdir.dict[1] = 1
+    tmpdir.dict[0] = 0
+    tmpdir.dict[2] = 2
 
-  assert tmpdir.dict.popItem()[1] == 0
-  assert tmpdir.dict.popItem()[1] == 1
-  assert tmpdir.dict.popItem()[1] == 2
+    assert tmpdir.dict.popItem()[1] == 0
+    assert tmpdir.dict.popItem()[1] == 1
+    assert tmpdir.dict.popItem()[1] == 2
+
 
 def test_CustomSortFunc(tmpdir):
-  """Use custom sort fuction to change order of popped items"""
-  def sortFunc(item):
-    if (item[1] == 2):
-      return 0
-    else:
-      return item[1] + 10
+    """Use custom sort fuction to change order of popped items"""
 
-  tmpdir.dict = SortedDict(getSortValue = sortFunc)
-  tmpdir.dict[1] = 1
-  tmpdir.dict[0] = 0
-  tmpdir.dict[2] = 2
+    def sortFunc(item):
+        if item[1] == 2:
+            return 0
+        else:
+            return item[1] + 10
 
-  assert tmpdir.dict.popItem()[1] == 2
-  assert tmpdir.dict.popItem()[1] == 0
-  assert tmpdir.dict.popItem()[1] == 1
+    tmpdir.dict = SortedDict(getSortValue=sortFunc)
+    tmpdir.dict[1] = 1
+    tmpdir.dict[0] = 0
+    tmpdir.dict[2] = 2
+
+    assert tmpdir.dict.popItem()[1] == 2
+    assert tmpdir.dict.popItem()[1] == 0
+    assert tmpdir.dict.popItem()[1] == 1
+
 
 def test_DeleteItemInDict(tmpdir):
-  """Delete an item in the dict"""
-  tmpdir.dict[0] = 0
-  del(tmpdir.dict[0])
-  assert not tmpdir.dict.hasKey(0)
+    """Delete an item in the dict"""
+    tmpdir.dict[0] = 0
+    del tmpdir.dict[0]
+    assert not tmpdir.dict.hasKey(0)
+
 
 def test_UpdateItem(tmpdir):
-  """Update an item and check it, make sure it stays in sorted order"""
-  tmpdir.dict[0] = 0
-  assert tmpdir.dict[0] == 0
+    """Update an item and check it, make sure it stays in sorted order"""
+    tmpdir.dict[0] = 0
+    assert tmpdir.dict[0] == 0
 
-  tmpdir.dict[0] = 15
-  assert tmpdir.dict[0] == 15
+    tmpdir.dict[0] = 15
+    assert tmpdir.dict[0] == 15
+
 
 def test_UpdateItemResort(tmpdir):
-  """Update an item and check that it got sorted into its proper spot"""
-  def sortFunc(item):
-    return item[1]
+    """Update an item and check that it got sorted into its proper spot"""
 
-  tmpdir.dict = SortedDict(getSortValue = sortFunc)
-  tmpdir.dict[0] = 0
-  tmpdir.dict[1] = 1
-  tmpdir.dict[0] = 15
+    def sortFunc(item):
+        return item[1]
 
-  assert tmpdir.dict.popItem()[1] == 1
+    tmpdir.dict = SortedDict(getSortValue=sortFunc)
+    tmpdir.dict[0] = 0
+    tmpdir.dict[1] = 1
+    tmpdir.dict[0] = 15
+
+    assert tmpdir.dict.popItem()[1] == 1
+
+def test_GetKeys(tmpdir):
+    """Get Keys from dict"""
+
+    tmpdir.dict[0] = 0
+    tmpdir.dict[1] = 1
+    tmpdir.dict[2] = 2
+
+    expected = [0,1,2]
+    actual = tmpdir.dict.getKeys()
+    assert actual == expected
+
+def test_GetSortedKeys(tmpdir):
+    """Get Keys from dict that should be sorted"""
+
+    def sortFunc(item):
+        return item[1]
+
+    tmpdir.dict = SortedDict(getSortValue=sortFunc)
+    tmpdir.dict[0] = 2
+    tmpdir.dict[1] = 1
+    tmpdir.dict[2] = 0
+
+    expected = [2,1,0]
+    actual = tmpdir.dict.getKeys()
+    assert actual == expected
+
+def test_GetSortedKeysSameValue(tmpdir):
+    """Get Keys from dict that are sorted but same value so same as input"""
+
+    def sortFunc(item):
+        return item[1]
+
+    tmpdir.dict = SortedDict(getSortValue=sortFunc)
+    tmpdir.dict[0] = 0
+    tmpdir.dict[1] = 0
+    tmpdir.dict[2] = 0
+
+    expected = [0,1,2]
+    actual = tmpdir.dict.getKeys()
+    assert actual == expected
