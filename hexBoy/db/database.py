ab8af83827d7ec4d6f5bdda7e0a0932a3708f75d
Brandon Godley <brandongodley2@gmail.com>
2023-05-13 12:20:41 -0700

    feat: started a little test database

--- hexBoy/db/database.py
+++ hexBoy/db/database.py
@@ -1,6 +1,30 @@
-import sqlite3
-from sqlalchemy import create_engine
+# import sqlite3
 
-engine = create_engine('sqlite:///hex_sqlite.db')
+from sqlalchemy import create_engine, inspect, text
 
-engine.table_names()
+connectionPath = 'hexBoy/db/hex_sqlite.db'
+connectionString = 'sqlite:///' + connectionPath
+
+engine = create_engine(connectionString, echo=True)
+print(type(engine))
+
+# print table names, I don't think I ever care unless to test I guess
+insp = inspect(engine)
+print(insp.get_table_names())
+
+
+with engine.connect() as con: 
+    result = con.execute(text("SELECT * FROM movie LIMIT 10"))
+
+    first_result = result.fetchone()
+    print(type(first_result))
+    print(first_result)
+    print(first_result['name'])
+
+    
+
+
+
+
+
+
