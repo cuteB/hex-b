cc14924f29df79faac27687f77ac0e153b97cdb1
Brandon Godley <brandongodley2@gmail.com>
2023-05-15 10:00:10 -0700

    feat: moving to sqlalchemy 2

--- hexBoy/db/database.py
+++ hexBoy/db/database.py
@@ -1,30 +1,11 @@
 # import sqlite3
 
-from sqlalchemy import create_engine, inspect, text
+from typing import List, Optional
+from sqlalchemy import create_engine, ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 connectionPath = 'hexBoy/db/hex_sqlite.db'
 connectionString = 'sqlite:///' + connectionPath
 
 engine = create_engine(connectionString, echo=True)
 print(type(engine))
-
-# print table names, I don't think I ever care unless to test I guess
-insp = inspect(engine)
-print(insp.get_table_names())
-
-
-with engine.connect() as con: 
-    result = con.execute(text("SELECT * FROM movie LIMIT 10"))
-
-    first_result = result.fetchone()
-    print(type(first_result))
-    print(first_result)
-    print(first_result['name'])
-
-    
-
-
-
-
-
-
